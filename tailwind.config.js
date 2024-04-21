60f3ef37b48fb0528dff5f065b94561ba3094cf9
lourencosalvador <lourencocardoso007@gmail.com>
2024-04-21 19:57:04 +0100

    :tade: iniciando o projeto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './App/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
